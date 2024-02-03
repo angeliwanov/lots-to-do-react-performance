156e12cb48f582c7481eb975457aae7472a3de62
Angel Ivanov <angel.iwanov@gmail.com>
2024-02-03 13:11:50 +0200

    useDeferredValue

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -3,29 +3,27 @@ import { initialFilters } from '../features/filters';
 import Filters from './filters';
 import Tasks from './tasks';
 import useFilters, { filterTasks } from '../lib/filter-tasks';
-import { useMemo, useTransition } from 'react';
+import { useMemo, useDeferredValue } from 'react';
 
 const Application = () => {
   const [tasks] = useTasks();
   const [filters, setFilter] = useFilters(initialFilters);
-  const [filterInput, setFilterInput] = useFilters(initialFilters);
-  const [isPending, startTransition] = useTransition();
+  const deferredFilters = useDeferredValue(filters);
 
   const visibleTasks = useMemo(
-    () => filterTasks(tasks, filters),
-    [tasks, filters],
+    () => filterTasks(tasks, deferredFilters),
+    [tasks, deferredFilters],
   );
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFilterInput(name, value);
-    startTransition(() => setFilter(name, value));
+    setFilter(name, value);
   };
 
   return (
     <main>
-      {isPending && <p>Loading...</p>}
-      <Filters filters={filterInput} onChange={handleChange} />
+      {deferredFilters !== filters && <p>Loading...</p>}
+      <Filters filters={filters} onChange={handleChange} />
       <Tasks tasks={visibleTasks} />
     </main>
   );
